f2fb0a6787c7cc8e13823a5101eb9caed1188d74
NgoTanKhoa21042001 <75767905+NgoTanKhoa21042001@users.noreply.github.com>
2021-03-23 16:09:30 +0700

    Add files via upload

---
+++ naruto.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./btn.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body>
+  <div class="content">
+    <div class="button__price">
+      <h2 class="header">Inspection</h2>
+      <p class="content">Đây là bảng giá thị trường cho chứng khoán</p>
+      <img src="./ExpressAdv_ENU/w3_akatsuki/assets/css/Akatsuki_files/Akatsuki12_files/Làng cát.jpg" alt="" class="product">
+      <p class="number_price">$300.000</p> 
+      <ul>
+        <li>Auto Product Liability</li>
+        <li>Crib Recall</li>
+        <li>Defective Airbags</li>
+        <li>Defective Medical Device</li>
+      </ul>
+      <div class="button_text">
+        <button type="button" class="btn btn-primary btn-lg">Large button</button>
+      </div>
+    </div>
+
+    <div class="button__price">
+      <h2 class="header">Inspection</h2>
+      <p class="content">Đây là bảng giá thị trường cho chứng khoán</p>
+      <img src="./ExpressAdv_ENU/w3_akatsuki/assets/css/Akatsuki_files/Akatsuki12_files/Làng cát.jpg" alt="" class="product">
+      <p class="number_price">$300.000</p> 
+      <ul>
+        <li>Auto Product Liability</li>
+        <li>Crib Recall</li>
+        <li>Defective Airbags</li>
+        <li>Defective Medical Device</li>
+      </ul>
+      <div class="button_text">
+        <button type="button" class="btn btn-primary btn-lg">Large button</button>
+      </div>
+    </div>
+
+    <div class="button__price">
+      <h2 class="header">Inspection</h2>
+      <p class="content">Đây là bảng giá thị trường cho chứng khoán</p>
+      <img src="./ExpressAdv_ENU/w3_akatsuki/assets/css/Akatsuki_files/Akatsuki12_files/Làng cát.jpg" alt="" class="product">
+      <p class="number_price">$300.000</p> 
+      <ul>
+        <li>Auto Product Liability</li>
+        <li>Crib Recall</li>
+        <li>Defective Airbags</li>
+        <li>Defective Medical Device</li>
+      </ul>
+      <div class="button_text">
+        <button type="button" class="btn btn-primary btn-lg">Large button</button>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
